41749f158d4cd484657528ccddc567e4aa5582d0
hossamshawaqfeh <hossamshawaqfehh@gmail.com>
2026-04-21 12:54:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '')
+export default defineConfig({
+  base: '/hossamshawaqfeh2/',
 
-  return {
-    base: '/HOSSAM-Shawaqfeh/',
-    plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  }
+  },
 })
